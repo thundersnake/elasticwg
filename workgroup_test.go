6507bad5f6830b8c25283fb1b9467344991b52ed
Loic Blot <loic.blot@unix-experience.fr>
2018-08-23 22:51:33 +0200

    Add more unittests & a setter on Workgroup.indexMapping

--- workgroup_test.go
+++ workgroup_test.go
@@ -84,11 +84,65 @@ func TestNewWorkgroup(t *testing.T) {
 	)
 }
 
-func TestWorkgroupSetOnProduceCallback(t *testing.T) {
+func TestWorkgroup_SetOnProduceCallback(t *testing.T) {
 	wg := NewWorkgroup(esURL, "test_index", "doc_type_test", &testProducer{}, 10, 500, gTestLogger)
-	wg.SetOnProduceCallback(func(uint64) {
-		gTestLogger.Info("test")
+	wg.SetOnProduceCallback(func(a uint64) {
+		gTestLogger.Info("test %d", a)
 	})
 
 	assert.NotNil(t, wg.p.onProduceCallback)
+}
+
+func TestWorkgroup_SetFailureCallback(t *testing.T) {
+	wg := NewWorkgroup(esURL, "test_index", "doc_type_test", &testProducer{}, 10, 500, gTestLogger)
+	wg.SetFailureCallback(func() {
+		gTestLogger.Info("test")
+	})
+
+	assert.NotNil(t, wg.onFailureCallback)
+}
+
+func TestWorkgroup_SetFinishCallback(t *testing.T) {
+	wg := NewWorkgroup(esURL, "test_index", "doc_type_test", &testProducer{}, 10, 500, gTestLogger)
+	wg.SetFinishCallback(func() {
+		gTestLogger.Info("test")
+	})
+
+	assert.NotNil(t, wg.onFinishCallback)
+}
+
+func TestWorkgroup_SetIndexMapping(t *testing.T) {
+	wg := NewWorkgroup(esURL, "test_index", "doc_type_test", &testProducer{}, 10, 500, gTestLogger)
+	indexMapping := make(map[string]interface{}, 0)
+	wg.SetIndexMapping(indexMapping)
+
+	assert.NotNil(t, wg.indexMapping)
+}
+
+func TestWorkgroup_SetOnProductionFinishedCallback(t *testing.T) {
+	wg := NewWorkgroup(esURL, "test_index", "doc_type_test", &testProducer{}, 10, 500, gTestLogger)
+	wg.SetOnProductionFinishedCallback(func(a uint64) {
+		gTestLogger.Infof("test: %d", a)
+	})
+
+	assert.NotNil(t, wg.p.onProductionFinishedCallback)
+}
+
+func TestWorkgroup_SetOnPushCallback(t *testing.T) {
+	wg := NewWorkgroup(esURL, "test_index", "doc_type_test", &testProducer{}, 10, 500, gTestLogger)
+	wg.SetOnPushCallback(func(a int) {
+		gTestLogger.Infof("test: %d", a)
+	})
+
+	assert.NotNil(t, wg.onPushCallback)
+}
+
+func TestWorkgroup_SetStartupCallback(t *testing.T) {
+	wg := NewWorkgroup(esURL, "test_index", "doc_type_test", &testProducer{}, 10, 500, gTestLogger)
+	wg.SetStartupCallback(func() bool {
+		gTestLogger.Info("test")
+		return true
+	})
+
+	assert.NotNil(t, wg.onStartupCallback)
 }
